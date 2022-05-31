50f1374bcfc411d8f5f5a3e4f0f55506c89ef1d4
iorn <wmt.tkn.121@gmail.com>
2022-05-31 17:20:21 +0900

    change theme

--- src/index.js
+++ src/index.js
@@ -4,17 +4,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SoundApp from './soundApp.jsx';
 import Header from './header';
-import { styled } from '@mui/material/styles';
 
-const BG=styled("div")(({theme})=>({
-    backgroundColor: theme.palette.background.paper,
-}));
+import DocumentMeta from 'react-document-meta';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+
+const meta = {
+            title: 'Chord Progressor',
+            description: 'コード進行をテストできるアプリ by React',
+            meta: {
+              charset: 'utf-8',
+              name: {
+                keywords: 'react,meta,chord,music'
+              }
+            }
+          };
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#38b48b',
+      contrastText: '#fcfcfc',
+    },
+    text: { primary: '#38b48b' },
+  },
+});
 
 
 ReactDOM.render(
-    <BG>
+    <ThemeProvider theme={theme}>
+        <DocumentMeta {...meta} />
         <Header />
         <SoundApp />
-    </BG>,
+    </ThemeProvider>,
     document.getElementById('root')
 );
