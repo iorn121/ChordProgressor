3f9a66f453a42affb588c713fce3e03c8aaae7fc
iorn <wmt.tkn.121@gmail.com>
2022-05-31 16:37:24 +0900

    change width

--- src/soundApp.jsx
+++ src/soundApp.jsx
@@ -54,14 +54,14 @@ export default function SoundApp() {
   useEffect(() =>{getChord()},[]);
   return (
     <Grid container direction="column" alignContent="center" justifyContent="center">
-    <Card variant="outlined" sx={{m:2,width: 450}}>
+    <Card variant="outlined" sx={{m:2,width: 400}}>
     <CardContent>
       <Key setKey={setKey} />
       <Chord setChord={setChord} />
       <Octave setOctave={setOctave} />
       <Length setLength={setLength} />
       <Typography variant="h4" sx={{fontWeight: "bold",textAlign: "center",m:2}}>
-        {key[1]+octave} / {chord[1]} / {length[1]}
+        {key[1]+octave}/{chord[1]}/{length[1]}
       </Typography>
     </CardContent>
     <CardActions>
